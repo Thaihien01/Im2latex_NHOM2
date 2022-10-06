2136d048ec3f2552eebd24797a613829c1e5fd40
unknown <hienthai1234thptdh@gmail.com>
2022-10-07 00:06:33 +0700

    change cv

--- model/model.py
+++ model/model.py
@@ -17,11 +17,7 @@ class Im2LatexModel(nn.Module):
         super(Im2LatexModel, self).__init__()
 
         self.cnn_encoder = nn.Sequential(
-            nn.Conv2d(3, 32, 3, 1, 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2, 1),
-
-            nn.Conv2d(32, 64, 3, 1, 1),
+            nn.Conv2d(3, 64, 3, 1, 1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2, 1),
 
@@ -30,10 +26,6 @@ class Im2LatexModel(nn.Module):
             nn.MaxPool2d(2, 2, 1),
 
             nn.Conv2d(128, 256, 3, 1, 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2, 1),
-
-            nn.Conv2d(256, 256, 3, 1, 1),
             nn.ReLU(),
             nn.Conv2d(256, 256, 3, 1, 1),
             nn.ReLU(),
